397b0b5302cfe3361c18c34b5035c9bc27fee886
Pranav Kad <pranavkad19@gmail.com>
2024-08-29 10:40:55 +0530

    Commiting Model Employee.cs

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSON_Type_Convert.Models
+{
+    // "Employee" is the model/class name 
+    public class Employee
+    {
+        //Declaring the properties
+        public string name { get; set; }    // Name of the employee
+        public DateTime birthdate { get; set; }  // Birthdate of the employee
+        public double salary {  get; set; }      // Salary of the employee
+        public string address {  get; set; }     // Address of the employee
+        public bool married {  get; set; }       // Marital status of the employee 
+
+    }
+}
